fdcdee53e8d70c3eb71bddb8df6e7db99058bfb6
tolgaand <tolgaand@yandex.com>
2022-08-25 22:55:35 +0300

    Create counter-create and counter-increment methods

--- programs/solana-counter/src/lib.rs
+++ programs/solana-counter/src/lib.rs
@@ -6,10 +6,37 @@ declare_id!("5U44h9RTCKSm1vCTn6pjhS8xQofxFv27tjzh1zxqD58k");
 pub mod solana_counter {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create(ctx: Context<Create>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count = 0;
+
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count += 1;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer=user, space = 16 + 16)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub count: u64,
+}
--- tests/solana-counter.ts
+++ tests/solana-counter.ts
@@ -1,6 +1,9 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { assert } from "chai";
 import { SolanaCounter } from "../target/types/solana_counter";
+
+let _baseAccount = null;
 
 describe("solana-counter", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +11,43 @@ describe("solana-counter", () => {
 
   const program = anchor.workspace.SolanaCounter as Program<SolanaCounter>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Create a count", async () => {
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    await program.methods
+      .create()
+      .accounts({
+        baseAccount: baseAccount.publicKey,
+        user: anchor.AnchorProvider.env().publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([baseAccount])
+      .rpc();
+
+    const account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+
+    console.log("Count?", account.count.toString());
+    assert.ok(account.count.toString() === "0");
+
+    _baseAccount = baseAccount;
+  });
+
+  it("Increment a count", async () => {
+    const baseAccount = _baseAccount;
+    await program.methods
+      .increment()
+      .accounts({
+        baseAccount: baseAccount.publicKey,
+      })
+      .rpc();
+
+    const account = await program.account.baseAccount.fetch(
+      baseAccount.publicKey
+    );
+
+    console.log("Count 1:", account.count.toString());
+    assert.ok(account.count.toString() === "1");
   });
 });
